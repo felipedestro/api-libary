9230e9cd93e2f820b68a23f1be7b571332d35e0a
Felipe Destro <felipe.s.destro@gmail.com>
2024-04-12 12:09:30 +0000

    edit: data-source entities

--- dist/data-source.js
+++ dist/data-source.js
@@ -4,10 +4,11 @@ exports.AppDataSource = void 0;
 require("dotenv/config");
 require("reflect-metadata");
 var typeorm_1 = require("typeorm");
+var Book_1 = require("../src/entities/Book");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL,
-    entities: ["../**/entities/*.{ts, js}"],
+    entities: [Book_1.Book],
     migrations: ["/../**/migrations/*.{ts, js}"],
 });
 //# sourceMappingURL=data-source.js.map
